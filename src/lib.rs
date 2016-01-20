4074313ea37959f08914927e06dcddfadead375e
Michele Bertasi <michele.bertasi@gmail.com>
2016-01-20 15:58:35 +0000

    Added buffer to handle reads from sig job

--- src/lib.rs
+++ src/lib.rs
@@ -31,6 +31,9 @@ pub type Result<T> = std::result::Result<T, Error>;
 pub struct Signature<R: Read> {
     old: R,
     job: *mut raw::rs_job_t,
+    buf: Vec<u8>,
+    pos: usize,
+    cap: usize,
 }
 
 impl<R: Read> Signature<R> {
@@ -39,23 +42,24 @@ impl<R: Read> Signature<R> {
                strong_len: usize,
                sig_magic: MagicNumber)
                -> Result<Self> {
-        unsafe {
-            let job = raw::rs_sig_begin(new_block_len, strong_len, sig_magic.as_raw());
-            if job.is_null() {
-                return Err(Error::BadMagic);
-            }
-            Ok(Signature {
-                old: old,
-                job: job,
-            })
+        let job = unsafe { raw::rs_sig_begin(new_block_len, strong_len, sig_magic.as_raw()) };
+        if job.is_null() {
+            return Err(Error::BadMagic);
         }
+        Ok(Signature {
+            old: old,
+            job: job,
+            buf: vec![0; raw::RS_DEFAULT_BLOCK_LEN],
+            pos: 0,
+            cap: 0,
+        })
     }
 }
 
 impl<R: Read> Drop for Signature<R> {
     fn drop(&mut self) {
+        assert!(!self.job.is_null());
         unsafe {
-            assert!(!self.job.is_null());
             raw::rs_job_free(self.job);
         }
     }
