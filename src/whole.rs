f66cfad1b8eda4a136ac4919b78ff603e4f29ce6
Michele Bertasi <michele.bertasi@gmail.com>
2016-02-01 09:28:37 +0000

    Added documentation for whole file APIs
    Closes #3.

--- src/whole.rs
+++ src/whole.rs
@@ -1,7 +1,26 @@
+//! Process whole files with a single call
+//!
+//! Provides functions to compute signatures, delta and patches with a single call. Those functions
+//! are useful if the application prefers to process whole files instead of using fine-grained APIs
+//! over IO.
+//!
+//! If fine-grained control over IO is necessary, it is provided by `Signature`, `Delta` and
+//! `Patch` structs.
+
 use std::io::{self, Read, Seek, Write};
 use super::*;
 
 
+/// Generates the signature of a basis input, and writes it out to an output stream.
+///
+/// This function will consume the given input stream and attempt to write the resulting signature
+/// to the given output. In case of success, the number of bytes written is returned, otherwise
+/// an error is reported.
+///
+/// The accepted arguments, among the input and output streams, are:
+/// * `block_len`: the block size for signature generation, in bytes;
+/// * `strong_len`: the truncated length of strong checksums, in bytes;
+/// * `sig_type`: the signature format to be used.
 pub fn sig<R, W: ?Sized>(input: R,
                          output: &mut W,
                          block_len: usize,
@@ -16,6 +35,16 @@ pub fn sig<R, W: ?Sized>(input: R,
     Ok(written)
 }
 
+
+/// Generates a delta between a signature and a new file streams.
+///
+/// This function will consume the new file and base signature inputs and writes to the given
+/// output the delta between them. In case of success, the number of bytes written is returned,
+/// otherwise an error is reported. The `new` parameter is the input stream representing a possibly
+/// modified file with respect to some base, for which its signature is provided as `base_sig`
+/// parameter.
+///
+/// To generate a signature, see the `sig` function, or the `Signature` struct.
 pub fn delta<R, S, W: ?Sized>(new: R, base_sig: S, output: &mut W) -> Result<u64>
     where R: Read,
           S: Read,
@@ -26,6 +55,17 @@ pub fn delta<R, S, W: ?Sized>(new: R, base_sig: S, output: &mut W) -> Result<u64
     Ok(written)
 }
 
+
+/// Applies a patch, relative to a basis, into a new file.
+///
+/// This function will consume the base file and the new file delta inputs and writes to the given
+/// output the patched input. In case of success, the number of bytes written is returned,
+/// otherwise an error is reported. The `base` parameter is the input stream representing the base
+/// file from which apply the patch. This stream must be seekable. The `delta` parameter is a
+/// stream containing the delta between the base file and the new one. The output parameter will
+/// be used to write the output.
+///
+/// To generate a delta, see the `delta` function, or the `Delta` struct.
 pub fn patch<B, D, W: ?Sized>(base: B, delta: D, output: &mut W) -> Result<u64>
     where B: Read + Seek,
           D: Read,
