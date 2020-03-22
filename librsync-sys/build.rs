67df1f5b1bef8516b945a8f3ddb6725c84c25922
Michele Bertasi <michele.bertasi@gmail.com>
2020-03-22 19:57:40 +0100

    Upgrade librsync-sys.

--- librsync-sys/build.rs
+++ librsync-sys/build.rs
@@ -1,4 +1,4 @@
-extern crate gcc;
+extern crate cc;
 
 use std::env;
 use std::path::Path;
@@ -7,7 +7,7 @@ fn main() {
     let target = env::var("TARGET").unwrap();
     let windows = target.contains("windows");
 
-    let mut cfg = gcc::Config::new();
+    let mut cfg = cc::Build::new();
 
     if windows {
         cfg.define("_WIN32", None);
@@ -20,36 +20,38 @@ fn main() {
     };
 
     cfg.include(cfg_dir)
-       .include("librsync/static")
-       .include("librsync/src")
-       .define("STDC_HEADERS", Some("1"))
-       .file("librsync/static/prototab.c")
-       .file("librsync/src/base64.c")
-       .file("librsync/src/buf.c")
-       .file("librsync/src/checksum.c")
-       .file("librsync/src/command.c")
-       .file("librsync/src/delta.c")
-       .file("librsync/src/emit.c")
-       .file("librsync/src/fileutil.c")
-       .file("librsync/src/hex.c")
-       .file("librsync/src/job.c")
-       .file("librsync/src/mdfour.c")
-       .file("librsync/src/mksum.c")
-       .file("librsync/src/msg.c")
-       .file("librsync/src/netint.c")
-       .file("librsync/src/patch.c")
-       .file("librsync/src/readsums.c")
-       .file("librsync/src/rollsum.c")
-       .file("librsync/src/scoop.c")
-       .file("librsync/src/search.c")
-       .file("librsync/src/stats.c")
-       .file("librsync/src/stream.c")
-       .file("librsync/src/sumset.c")
-       .file("librsync/src/trace.c")
-       .file("librsync/src/tube.c")
-       .file("librsync/src/util.c")
-       .file("librsync/src/version.c")
-       .file("librsync/src/whole.c")
-       .file("librsync/src/blake2b-ref.c")
-       .compile("librsync.a");
+        .include("librsync/static")
+        .include("librsync/src")
+        .include("librsync/src/blake2")
+        .define("STDC_HEADERS", Some("1"))
+        .file("librsync/src/base64.c")
+        .file("librsync/src/buf.c")
+        .file("librsync/src/checksum.c")
+        .file("librsync/src/command.c")
+        .file("librsync/src/delta.c")
+        .file("librsync/src/emit.c")
+        .file("librsync/src/fileutil.c")
+        .file("librsync/src/hashtable.c")
+        .file("librsync/src/hex.c")
+        .file("librsync/src/isprefix.c")
+        .file("librsync/src/job.c")
+        .file("librsync/src/mdfour.c")
+        .file("librsync/src/mksum.c")
+        .file("librsync/src/msg.c")
+        .file("librsync/src/netint.c")
+        .file("librsync/src/patch.c")
+        .file("librsync/src/prototab.c")
+        .file("librsync/src/readsums.c")
+        .file("librsync/src/rollsum.c")
+        .file("librsync/src/scoop.c")
+        .file("librsync/src/stats.c")
+        .file("librsync/src/stream.c")
+        .file("librsync/src/sumset.c")
+        .file("librsync/src/trace.c")
+        .file("librsync/src/tube.c")
+        .file("librsync/src/util.c")
+        .file("librsync/src/version.c")
+        .file("librsync/src/whole.c")
+        .file("librsync/src/blake2/blake2b-ref.c")
+        .compile("librsync.a");
 }
